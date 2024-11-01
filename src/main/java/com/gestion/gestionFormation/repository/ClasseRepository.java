42ae19f0d93dbb7b7b1752d7b5911283a77fcb36
ATalemsi <mohamadtalemsi@gmail.com>
2024-11-01 12:23:47 +0100

    GS-74 Gestion des classe

--- src/main/java/com/gestion/gestionFormation/repository/ClasseRepository.java
+++ src/main/java/com/gestion/gestionFormation/repository/ClasseRepository.java
@@ -1,7 +1,23 @@
 package com.gestion.gestionFormation.repository;
 
 import com.gestion.gestionFormation.model.Classe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ClasseRepository extends JpaRepository<Classe, Long> {
+
+    List<Classe> findByNom(String nom);
+
+    List<Classe> findByNomAndNumSalle(String nom, Integer  numSalle);
+
+    @Query("SELECT c FROM Classe c WHERE c.nom LIKE %:nom% AND c.numSalle = :numSalle")
+    List<Classe> searchByNomAndNumSalle(@Param("nom") String nom, @Param("numSalle") Integer  numSalle);
+
+    Page<Classe> findByNom(String nom, Pageable pageable);
+
 }
--- src/main/java/com/gestion/gestionFormation/service/ClasseService.java
+++ src/main/java/com/gestion/gestionFormation/service/ClasseService.java
@@ -1,6 +1,8 @@
 package com.gestion.gestionFormation.service;
 
 import com.gestion.gestionFormation.model.Classe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +12,10 @@ public interface ClasseService {
     Classe updateClasse(Long id, Classe classe);
     Optional<Classe> getClasse(Long id);
     String deleteClasse(Long id);
-    List<Classe> getAllClasses();
+    Page<Classe> getAllClasses(Pageable pageable);
+
+    List<Classe> findByNom(String nom);
+    List<Classe> findByNomAndNumSalle(String nom, int numSalle);
+    List<Classe> searchByNomAndNumSalle(String nom, int numSalle);
+    Page<Classe> getClassesByNom(String nom, Pageable pageable);
 }
